add92325e5a89ff0d9db63ed22564aebcfa082c2
Simon AM <purrcat259@users.noreply.github.com>
2018-02-24 14:28:15 +0100

    Added slice growing tests

--- models/slice.py
+++ models/slice.py
@@ -1,6 +1,6 @@
 
 class Slice:
-    def __init__(self, data, top_left, rows=1, cols=1):
+    def __init__(self, data, top_left, rows=1, columns=1):
         self.rows = rows
         self.columns = columns
         self.top_left = top_left
@@ -13,45 +13,47 @@ class Slice:
         for row in range(self.top_left[0], self.top_left[0] + self.rows):
             for col in range(self.top_left[1], self.top_left[1] + self.columns):
                 self.addIngredient(data, row, col)
-    
-    def addIngredient(self, data, row, col):
-        ing = data[row][col]
-        if(ing == "M"):
-            self.addMushroom(1)
-        else if(ing == "T")
-            self.addTomato(1)    
-
-    def addMushroom(self):
-        self.mushroom++
-
-    def addTomato(self):
-        self.tomato++
 
     def grow(self, data, dir):
-        if(dir == 0):
+        if dir == 0:
             return self.growRow(data)
-        else if(dir == 1):
+        elif dir == 1:
             return self.growCol(data)
 
     def growRow(self, data):
-        t_row = self.top_left[0] + self.rows + 1
-        r_col = self.top_left[1] + self.cols
+        t_row = self.top_left[0] + self.rows - 1 + 1
+        r_col = self.top_left[1] + self.columns
 
-        for cell_col in range(top_left[1], r_col)
-            self.addIngredient(data[t_row][cell_col])  
+        for cell_col in range(self.top_left[1], r_col):
+            self.addIngredient(data, t_row, cell_col)
 
-        self.rows++
+        self.rows += 1
 
     def growCol(self, data):
         br_row = self.top_left[0] + self.rows
-        t_col = self.top_left[1] + self.cols + 1
+        t_col = self.top_left[1] + self.columns - 1 + 1
 
-        for cell_row in range(top_left[0], br_row)
-            self.addIngredient(data[cell_row][t_col])
+        for cell_row in range(self.top_left[0], br_row):
+            self.addIngredient(data, cell_row, t_col)
             
-        self.cols++
+        self.columns += 1
     
     def growBoth(self, data):
         self.grow(data, 0)
         self.grow(data, 1)
-        
+
+    def addIngredient(self, data, row, col):
+        ing = data[row][col]
+        if ing == "M":
+            self.addMushroom()
+        else:
+            self.addTomato()
+
+    def addMushroom(self):
+        self.mushroom += 1
+
+    def addTomato(self):
+        self.tomato += 1
+
+    def size(self):
+        return self.rows * self.columns
--- tests/test_slice_growing.py
+++ tests/test_slice_growing.py
@@ -1,19 +1,60 @@
 from input import ExampleInput
 
-from process import Process
+from models.slice import Slice
+
+"""
+TTTTT
+TMMMT
+TTTTT
+"""
 
 example_input = ExampleInput()
 example_input.read_file()
 
-process = Process(example_input)
+data = example_input.data
 
 
 class TestSliceGrowing:
+    s = None
+
+    def setup_method(self):
+        self.s = Slice(data, (0, 0))
+
     def test_grow_row(self):
-        assert None is not None
+        assert 1 == self.s.size()
+        assert 1 == self.s.tomato
+        assert 0 == self.s.mushroom
+        self.s.growRow(data)
+        assert 2 == self.s.size()
+        assert 2 == self.s.tomato
+        assert 0 == self.s.mushroom
+        self.s.growRow(data)
+        assert 3 == self.s.size()
+        assert 3 == self.s.tomato
+        assert 0 == self.s.mushroom
 
     def test_grow_column(self):
-        assert None is not None
+        assert 1 == self.s.size()
+        assert 1 == self.s.tomato
+        assert 0 == self.s.mushroom
+        self.s.growCol(data)
+        assert 2 == self.s.size()
+        assert 2 == self.s.tomato
+        assert 0 == self.s.mushroom
+        self.s.growCol(data)
+        assert 3 == self.s.size()
+        assert 3 == self.s.tomato
+        assert 0 == self.s.mushroom
 
     def test_grow_both(self):
-        assert None is not None
+        assert 1 == self.s.size()
+        assert 1 == self.s.tomato
+        assert 0 == self.s.mushroom
+        self.s.growBoth(data)
+        assert 4 == self.s.size()
+        assert 3 == self.s.tomato
+        assert 1 == self.s.mushroom
+        self.s.growBoth(data)
+        assert 9 == self.s.size()
+        assert 7 == self.s.tomato
+        assert 2 == self.s.mushroom
