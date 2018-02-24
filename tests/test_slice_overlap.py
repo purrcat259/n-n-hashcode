705ab0c58c0d8eb6df6e53bff415309b497d8482
Simon AM <purrcat259@users.noreply.github.com>
2018-02-24 14:40:05 +0100

    Added overlapping test

---
+++ tests/test_slice_overlap.py
@@ -0,0 +1,30 @@
+from process import Process
+from input import ExampleInput
+from models.slice import Slice
+
+
+"""
+TTTTT
+TMMMT
+TTTTT
+"""
+
+example_input = ExampleInput()
+example_input.read_file()
+
+data = example_input.data
+
+
+class TestSliceOverlap:
+    p = None
+
+    def setup_method(self):
+        self.p = Process(example_input)
+
+    def test_returns_false_on_adjacent_slices(self):
+        self.p.slices.append(Slice(data, 0, 0))
+        assert False is self.p.overlap_exists(Slice(data, 1, 1))
+
+    def test_returns_true_on_overlapping_slices(self):
+        self.p.slices.append(Slice(data, 0, 0, 2, 2))
+        assert True is self.p.overlap_exists(Slice(data, 1, 1))
