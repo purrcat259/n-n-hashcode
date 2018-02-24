e78afc90a28299d43fce25453af276cec9ff8d2e
Simon AM <purrcat259@users.noreply.github.com>
2018-02-24 14:10:11 +0100

    Basic slice growing test

---
+++ tests/test_slice_growing.py
@@ -0,0 +1,19 @@
+from input import ExampleInput
+
+from process import Process
+
+example_input = ExampleInput()
+example_input.read_file()
+
+process = Process(example_input)
+
+
+class TestSliceGrowing:
+    def test_grow_row(self):
+        assert None is not None
+
+    def test_grow_column(self):
+        assert None is not None
+
+    def test_grow_both(self):
+        assert None is not None
